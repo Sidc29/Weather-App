7adcf7bf8082cd901068cc63472d065c87314a73
Shiddharth Choudhari <sidc29@protonmail.com>
2024-05-22 00:14:44 +0530

    refine loading skeleton

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,12 +1,16 @@
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-import { getTimeFormatted } from "../lib/utils";
 
-const CustomSkeletonTheme = ({ count, height, width }) => {
+const CustomSkeletonTheme = ({ count, height, width, borderRadius }) => {
   return (
     <SkeletonTheme baseColor="#202020" highlightColor="#444">
       <p>
-        <Skeleton count={count} height={height} width={width} />
+        <Skeleton
+          count={count}
+          height={height}
+          width={width}
+          borderRadius={borderRadius}
+        />
       </p>
     </SkeletonTheme>
   );
@@ -16,23 +20,60 @@ const Loading = () => {
   return (
     <div className="wrapper">
       <div className="container">
-        <div className="weather__header"></div>
         <div className="weather__body">
-          <CustomSkeletonTheme count={1} height={40} width={150} />
-          <CustomSkeletonTheme count={1} height={20} width={100} />
-          <CustomSkeletonTheme count={1} height={30} width={100} />
-          <CustomSkeletonTheme count={1} height={20} width={50} />
+          <CustomSkeletonTheme
+            count={1}
+            height={40}
+            width={200}
+            borderRadius={10}
+          />
+          <CustomSkeletonTheme
+            count={1}
+            height={15}
+            width={150}
+            borderRadius={30}
+          />
+          <CustomSkeletonTheme
+            count={1}
+            height={40}
+            width={120}
+            borderRadius={30}
+          />
+          <CustomSkeletonTheme
+            count={1}
+            height={50}
+            width={60}
+            borderRadius={10}
+          />
+          <p className="weather__temperature">
+            <CustomSkeletonTheme
+              count={1}
+              height={40}
+              width={100}
+              borderRadius={10}
+            />
+          </p>
           <div className="weather__minmax">
-            <CustomSkeletonTheme count={1} width={50} />
-            <CustomSkeletonTheme count={1} width={50} />
+            <CustomSkeletonTheme
+              count={1}
+              height={20}
+              width={80}
+              borderRadius={10}
+            />
+            <CustomSkeletonTheme
+              count={1}
+              height={20}
+              width={80}
+              borderRadius={10}
+            />
           </div>
         </div>
 
         <div className="weather__info">
-          <CustomSkeletonTheme count={1} height={60} />
-          <CustomSkeletonTheme count={1} height={60} />
-          <CustomSkeletonTheme count={1} height={60} />
-          <CustomSkeletonTheme count={1} height={60} />
+          <CustomSkeletonTheme count={1} height={80} borderRadius={10} />
+          <CustomSkeletonTheme count={1} height={80} borderRadius={10} />
+          <CustomSkeletonTheme count={1} height={80} borderRadius={10} />
+          <CustomSkeletonTheme count={1} height={80} borderRadius={10} />
         </div>
       </div>
     </div>
